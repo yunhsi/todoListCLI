b5a10fd333c9b15682d18eb7da270e3a3848328d
yunhsi <cles30102@gmail.com>
2021-08-26 05:34:56 +0800

    init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>
+      <input type="checkbox" v-model="$store.state.isComplete" @click="complete">
+      <p></p>
+      <button>x</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    newTodo: "",
+    isComplete:false,
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+});
